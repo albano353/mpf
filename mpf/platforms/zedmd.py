7b01f669a75912d5c1f6cec701da578a48aa14c6
JerOrc <jvspams-github@yahoo.fr>
2025-03-02 16:05:09 +0100

    add ZeDMD width, height and brightness

--- mpf/platforms/zedmd.py
+++ mpf/platforms/zedmd.py
@@ -7,6 +7,8 @@ Use libZeDMD Python extension
 https://github.com/PPUC/libzedmd-python-pybind11-extension
 
 """
+import logging
+
 import ctypes
 import pathlib
 
@@ -32,15 +34,20 @@ class ZeDmdPlatform(RgbDmdPlatform):
         """Initialize ZeDMD."""
         super().__init__(machine)
 
-        self.device = None        
+        self.device = None 
         self.config = None
 
-    async def initialize(self):
-        """Initialize platform."""
         self.config = self.machine.config_validator.validate_config(
             config_spec='zedmd',
             source=self.machine.config.get('zedmd', {})
         )
+
+        self._configure_device_logging_and_debug('ZeDMD',self.config)
+        
+
+    async def initialize(self):
+        """Initialize platform."""
+        
 
     def stop(self):
         """Stop platform."""
@@ -64,23 +71,34 @@ class ZeDmdDevice(DmdPlatformInterface):
 
     """A ZeDmd device."""
 
+    __slots__ = ["config","log","matrix"]
+
     def __init__(self, config):
         """Initialize ZeDmd device."""
         self.config = config
         self.matrix = ZeDMD_ext()
-
+        self.log = logging.getLogger('ZeDMDDevice')
+        
     def update(self, data):
         """Update DMD data."""
-        image = Image.frombytes('RGB', (128,32), data)
+        image = Image.frombytes('RGB', (self.config["width"],self.config["height"]), data)
         self.matrix.RenderRgb888(image)
 
-    def set_brightness(self, brightness: float):
+    def set_brightness(self, brightness):
         """Set brightness.
         Range is [0.0 ... 1.0].
         """
         if brightness < 0.0 or brightness > 1.0:
             raise AssertionError("Brightness has to be between 0 and 1.")
-        self.matrix.SetBrightness(round(brightness * 15))
+        new_brightness = round(brightness * 15)
+        self.log.info("Set brightness = {0:d}".format(new_brightness))
+        self.matrix.SetBrightness(new_brightness)
+
+        # If you prefer memorize brightness:
+        #if new_brightness != self.matrix.GetBrightness():
+        #  self.log.info("Set brightness = {0:d}".format(new_brightness))
+        #  self.matrix.SetBrightness(new_brightness)
+        #  self.matrix.SaveSettings()
 
     def stop(self):
         """Stop device."""
