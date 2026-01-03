fb58a3fed74f042fc7c85dbd2d4886b05bc4b7cb
Jim Henshaw <jimhenshaw@gmail.com>
2026-01-03 17:45:24 -0600

    updated to include missing space after , to be compliant with coding style.

--- mpf/platforms/virtual.py
+++ mpf/platforms/virtual.py
@@ -531,7 +531,7 @@ class VirtualLight(LightPlatformInterface):
         """Order lights by string."""
         self_address, self_offset = VirtualLight.split_light_address(self.number)
         other_address, other_offset = VirtualLight.split_light_address(other.number)
-        return (self_address,int(self_offset)) < (other_address, int(other_offset))
+        return (self_address, int(self_offset)) < (other_address, int(other_offset))
 
 
 class VirtualServo(ServoPlatformInterface):
